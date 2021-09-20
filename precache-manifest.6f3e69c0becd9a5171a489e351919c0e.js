677beee15985965bc7efffd83558994b5097b420
Alex <t9371100211@gmail.com>
2021-09-20 11:16:43 +0300

    Updates

---
+++ precache-manifest.6f3e69c0becd9a5171a489e351919c0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a67c77e44b3e53f25fec0efd2916eb5",
+    "url": "/Homeworks-It-Incubator/index.html"
+  },
+  {
+    "revision": "8005a0a3a115c1413505",
+    "url": "/Homeworks-It-Incubator/static/css/main.0373a7f8.chunk.css"
+  },
+  {
+    "revision": "f995187b6c5c71a32140",
+    "url": "/Homeworks-It-Incubator/static/js/2.c0912345.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Homeworks-It-Incubator/static/js/2.c0912345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8005a0a3a115c1413505",
+    "url": "/Homeworks-It-Incubator/static/js/main.ad82bcde.chunk.js"
+  },
+  {
+    "revision": "b6e9641b0f4c3ae136d6",
+    "url": "/Homeworks-It-Incubator/static/js/runtime-main.492192a6.js"
+  }
+]);
