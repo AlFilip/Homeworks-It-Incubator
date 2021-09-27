b8bc116258f2503f764f4a8d6b40553e9342e513
Alex <t9371100211@gmail.com>
2021-09-27 14:34:48 +0300

    sixth homework done

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from 'react-router-dom'
+import s from "./Header.module.css"
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+    const [isHover, setIsHover] = useState<boolean>(false)
+    const toggleHover = () => setIsHover(!isHover)
+    const headerClassName = `${s.header} ${isHover ? s.hovered : ''}`
 
+    return (
+        <div className={headerClassName} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+            <NavLink activeClassName={s.activeLink} to={'pre-junior'}>PreJunior</NavLink>
+            <NavLink activeClassName={s.activeLink} to={'junior'}>Junior</NavLink>
+            <NavLink activeClassName={s.activeLink} to={'junior-plus'}>Junior++</NavLink>
+            <span className={s.dots}>&#8285;</span>
         </div>
     )
 }
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={'error404'}>
+            <img src={require("../pages/404girl.jpg")} height={'400px'} width={'400px'} alt=""/>
+            <div className={'content404'}>
+                <span>404</span>
+                <div>Page not found!</div>
+                <div>But don't worry, there is navigation panel with actual links on left top corner=)</div>
+            </div>
         </div>
     )
 }
