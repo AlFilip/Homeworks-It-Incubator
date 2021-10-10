6ef9957fed0c00b4faa0b41dd7574a68a157e467
Alex <t9371100211@gmail.com>
2021-10-10 12:10:58 +0300

    Finished eighth home work

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,37 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {PeopleType} from "../HW8";
+
+export const homeWorkReducer = (state: PeopleType, action: HomeWorkActionsType): PeopleType => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+            switch (action.payload) {
+                case "up":
+                    return [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+                case "down":
+                    return [...state].sort((a, b) => a.name < b.name ? 1 : -1)
+                default:
+                    return state
+            }
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(f => f.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
+
+type HomeWorkActionsType = SortActionType | CheckActionType
+
+type SortActionType = ReturnType<typeof SortAC>
+export const SortAC = (payload: 'up' | 'down') => ({
+    type: 'sort',
+    payload
+} as const)
+
+type CheckActionType = ReturnType<typeof CheckAC>
+export const CheckAC = (payload: number) => ({
+    type: 'check',
+    payload
+} as const)
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import {homeWorkReducer} from '../homeWorkReducer'
+import {CheckAC, homeWorkReducer, SortAC} from '../homeWorkReducer'
 
 let initialState: any[] // need to fix any
 
@@ -15,18 +14,27 @@ beforeEach(() => {
 })
 
 test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+    const newState = homeWorkReducer(initialState, SortAC('up'))
 
     console.log(newState)
-    // expect(...).toBe(...)
+    expect(newState[0].name).toBe('Александр')
+    expect(initialState).not.toBe(newState)
+    expect(initialState.length).toBe(newState.length)
 })
 test('sort name down', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+    const newState = homeWorkReducer(initialState, SortAC('down'))
 
-
+    console.log(newState)
+    expect(newState[0].name).toBe('Кот')
+    expect(initialState).not.toBe(newState)
+    expect(initialState.length).toBe(newState.length)
 })
 test('check age 18', () => {
-    const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
+    const newState = homeWorkReducer(initialState, CheckAC(18))
 
-
+    console.log(newState)
+    expect(initialState).not.toBe(newState)
+    expect(newState.length).toBe(4)
+    expect(newState.find(f => f.name === 'Кот')).toBeUndefined()
+    expect(newState.find(f => f.name === 'Коля')).toBeUndefined()
 })
